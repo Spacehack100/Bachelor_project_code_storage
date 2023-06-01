30ad775fb0fde5c0d586c9277b95990b09167c27
Spacehack100 <jens.kersemans@telenet.be>
2023-06-01 12:55:19 +0200

    Better overall layout

--- Demo app with gui/main.py
+++ Demo app with gui/main.py
@@ -1,6 +1,4 @@
-import tkinter
 import requests
-import json
 
 from tkinter import *
 from tkinter import ttk
@@ -32,7 +30,7 @@ def RetrieveResults(*args):
             error.set('Error: HTTP code ' + str(result.status_code))
     else:
         print('No text or URL given')
-        error.set('Geen text of URL gegeven!')
+        error.set('Error: Geen tekst of URL gegeven!')
 
 # url = 'http://localhost:80/predictBody'
 # authenticationToken = ''
@@ -41,11 +39,9 @@ SentimentClasses = ['negatief','neutraal','positief']
 
 root = Tk()
 root.title("Demo classificeerder")
-inputFrame = ttk.Frame(root, width=200, height=100, borderwidth=2, relief='raised')
-resultFrame = ttk.Frame(root, width=200, height=100, borderwidth=2, relief='sunken')
-#classFrame = ttk.Frame(resultFrame)
-#confidenceFrame = ttk.Frame(resultFrame)
-#timeFrame = ttk.Frame(resultFrame)
+mainFrame = ttk.Frame(root, padding=5)
+inputFrame = ttk.Frame(mainFrame, borderwidth=2, relief='raised')
+resultFrame = ttk.Frame(mainFrame, borderwidth=2, relief='sunken')
 resultTableFrame = ttk.Frame(resultFrame)
 
 url = StringVar()
@@ -69,21 +65,18 @@ inputField = ttk.Entry(inputFrame, textvariable=text)
 retrieveResultsButton = ttk.Button(inputFrame,text='classificeer',command=RetrieveResults)
 
 PreProcessTimeOutput = ttk.Label(resultFrame, textvariable=resultPreProcessTime)
-subjectClassOutput = ttk.Label(resultTableFrame, textvariable=resultSubjectClass, borderwidth=2, relief='solid', width=11)
-subjectConfidenceOutput = ttk.Label(resultTableFrame, textvariable=resultSubjectConfidence, borderwidth=2, relief='solid', width=11)
-subjectTimeOutput = ttk.Label(resultTableFrame, textvariable=resultSubjectTime, borderwidth=2, relief='solid', width=11)
-SentimentClassOutput = ttk.Label(resultTableFrame, textvariable=resultSentimentClass, borderwidth=2, relief='solid', width=11)
-SentimentConfidenceOutput = ttk.Label(resultTableFrame, textvariable=resultSentimentConfidence, borderwidth=2, relief='solid', width=11)
-SentimentTimeOutput = ttk.Label(resultTableFrame, textvariable=resultSentimentTime, borderwidth=2, relief='solid', width=11)
+subjectClassOutput = ttk.Label(resultTableFrame, textvariable=resultSubjectClass, borderwidth=2, relief='solid', width=13,anchor='center')
+subjectConfidenceOutput = ttk.Label(resultTableFrame, textvariable=resultSubjectConfidence, borderwidth=2, relief='solid', width=13,anchor='center')
+subjectTimeOutput = ttk.Label(resultTableFrame, textvariable=resultSubjectTime, borderwidth=2, relief='solid', width=13,anchor='center')
+SentimentClassOutput = ttk.Label(resultTableFrame, textvariable=resultSentimentClass, borderwidth=2, relief='solid', width=13,anchor='center')
+SentimentConfidenceOutput = ttk.Label(resultTableFrame, textvariable=resultSentimentConfidence, borderwidth=2, relief='solid', width=13,anchor='center')
+SentimentTimeOutput = ttk.Label(resultTableFrame, textvariable=resultSentimentTime, borderwidth=2, relief='solid', width=13,anchor='center')
 errorOutput = ttk.Label(resultFrame, textvariable=error)
 
+mainFrame.grid(column=0,row=0)
 inputFrame.grid(column=0,row=0)
-
 resultFrame.grid(column=0,row=1)
 resultTableFrame.grid(column=0,row=1)
-#classFrame.grid(column=0,row=1)
-#confidenceFrame.grid(column=1,row=1)
-#timeFrame.grid(column=2,row=1)
 
 urlLabel.grid(column=0,row=0)
 urlEntry.grid(column=1,row=0)
@@ -96,17 +89,18 @@ retrieveResultsButton.grid(column=0,row=3,columnspan=2)
 
 ttk.Label(resultFrame, text='Tijd Data voorbereiden: ').grid(column=0,row=0)
 PreProcessTimeOutput.grid(column=1,row=0)
-ttk.Label(resultTableFrame, text='Onderwerp', borderwidth=2, relief='solid', width=11).grid(column=0,row=1)
-ttk.Label(resultTableFrame, text='Sentiment', borderwidth=2, relief='solid', width=11).grid(column=0,row=2)
-ttk.Label(resultTableFrame, text='Categorie', borderwidth=2, relief='solid', width=11).grid(column=1,row=0)
+ttk.Label(resultTableFrame, text='Onderwerp', borderwidth=2, relief='solid', width=13,anchor='center').grid(column=0,row=1)
+ttk.Label(resultTableFrame, text='Sentiment', borderwidth=2, relief='solid', width=13,anchor='center').grid(column=0,row=2)
+ttk.Label(resultTableFrame, text='Categorie', borderwidth=2, relief='solid', width=13,anchor='center').grid(column=1,row=0)
 subjectClassOutput.grid(column=1,row=1)
 SentimentClassOutput.grid(column=1,row=2)
-ttk.Label(resultTableFrame, text='Zekerheid (%)', borderwidth=2, relief='solid', width=11).grid(column=2,row=0)
+ttk.Label(resultTableFrame, text='Zekerheid (%)', borderwidth=2, relief='solid', width=13,anchor='center').grid(column=2,row=0)
 subjectConfidenceOutput.grid(column=2,row=1)
 SentimentConfidenceOutput.grid(column=2,row=2)
-ttk.Label(resultTableFrame, text='Tijd (s)', borderwidth=2, relief='solid', width=11).grid(column=3,row=0)
+ttk.Label(resultTableFrame, text='Tijd (s)', borderwidth=2, relief='solid', width=13,anchor='center').grid(column=3,row=0)
 subjectTimeOutput.grid(column=3,row=1)
 SentimentTimeOutput.grid(column=3,row=2)
 errorOutput.grid(column=0,row=2,columnspan=2)
 
+root.resizable(False, False)
 root.mainloop()
